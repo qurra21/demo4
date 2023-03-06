02f42ce4146bfcd83f2212dad48a0571304ab1e4
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2023-03-06 20:45:01 +0700

    test

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -12,11 +12,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello! world");
         stage.setScene(scene);
         stage.show();
     }
-
+//test jinh hz omg//
     public static void main(String[] args) {
         launch();
     }
